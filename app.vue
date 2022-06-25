b6601de0b1bb6eb3a67a430f208ffa8c37573b36
Sansrona <kingJaM00@yandex.ru>
2022-06-25 17:06:50 +0600

    add: base layout

--- app.vue
+++ app.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <div class=" mt-2.5 bg-red-900">
-      hi
-    </div>
-    <div class=" mt-2.5 bg-yellow-400">{{ counter.counter }} | {{counter.doubleCount}}</div>
-    <button class=" border-2 border-solid border-green-700 text-white bg-indigo-900" @click="counter.increment">Increment</button>
+  <div class=" max-w-full min-h-screen bg-gray-100">
+    <section class=" pt-7 text-center">
+      <h1 class=" text-4xl text-gray-800">Title is here</h1>
+    </section>
+    <section class=" w-9/12 max-w-lg mx-auto flex items-center justify-center bg-white mt-7 p-5 shadow rounded">
+      <form @submit.prevent >
+        <input type="text" placeholder="add a todo" class=" py-2 px-4  border border-blue-200 rounded focus:border-blue-300"  />
+        <button class=" transition-all duration-200 ml-3 px-4 py-2 border border-blue-200 rounded hover:bg-blue-400 hover:text-white">Add todo</button>
+      </form>
+    </section>
   </div>
 </template>
 <script setup>
